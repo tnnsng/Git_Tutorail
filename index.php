d756dc735d77997af26f335d92d712a096e283cb
tnnsng <6614210017@mut.ac.th>
2024-07-05 17:11:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
     echo "<h1>my repository</h1>" 
     echo "<h2>my name is tinnaphop</h2>" 
     echo "<h2>i like your hair</h2>" 
+    echo "<h1>my repository</h1>" 
+    echo "<h2>my name is tinnaphop</h2>" 
+    echo "<h2>i like your hair</h2>"
 ?>

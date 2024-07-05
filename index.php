94121cfc89012ddf5ebd22d83947dd636b1026de
tnnsng <6614210017@mut.ac.th>
2024-07-05 17:00:43 +0700

    commit3

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
     echo "<h1>my repository</h1>" 
     echo "<h2>my name is tinnaphop</h2>" 
+    echo "<h2>i like your hair</h2>" 
 ?>
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php 
     echo "<h1>menu</h1>" 
     echo "<h2>mahanakorn</h2>"
-    echo "<h2>tinnaphop nukonram</h2>"
+    echo "<h2>tinnaphop nukonram tintin</h2>"
 ?>
 

c7c88155b0fc5ae9e3fd4ef85cd37907d5e0d9ba
tnnsng <6614210017@mut.ac.th.com>
2024-07-12 17:16:45 +0700

    commit3

--- login.php
+++ login.php
@@ -3,10 +3,6 @@
     echo "<h1> User : </h1>"
     echo "<h1> Password : </h1>"
     echo "<h1> Name : </h1>"
-<<<<<<< HEAD
     echo "<h1> Age : </h1>"
-?>
-=======
     echo "</table>"
 ?>
->>>>>>> 4ecdd1b5862c9e2c29d58f14d714b547827e764a
